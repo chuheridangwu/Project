1f0c445dba2551d870fb2c1ad8d1962740d9bf04
董玉毛 <DYM@dongyumaodeMacBook-Pro.local>
2017-06-04 23:33:14 +0800

    first commit

---
+++ Project/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Project
+//
+//  Created by 董玉毛 on 2017/6/4.
+//  Copyright © 2017年 zzl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Project/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Project
+//
+//  Created by 董玉毛 on 2017/6/4.
+//  Copyright © 2017年 zzl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
